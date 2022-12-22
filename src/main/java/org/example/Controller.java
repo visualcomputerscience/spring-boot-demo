c8d4cb2e327414480d3c1894e14bae846ede2879
vcs <visualcomputerscience@gmail.com>
2022-12-22 23:49:00 +0200

    Add initial project

---
+++ src/main/java/org/example/Controller.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello back!";
+    }
+}

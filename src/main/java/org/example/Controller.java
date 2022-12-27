6cee04d7792b6ac373bf8d1e6f62f341518c10ae
vcs <visualcomputerscience@gmail.com>
2022-12-27 12:20:10 +0200

    Add new parameter controller method

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class Controller {
     @GetMapping("/hello")
     public String helloWorld() {
         return "Hello back updated 2!!";
+    }
+
+    @GetMapping("/hello/{param}")
+    public String helloWorldWithParam(@PathVariable Integer param) {
+        return "Hello back with para: " + param;
     }
 }
